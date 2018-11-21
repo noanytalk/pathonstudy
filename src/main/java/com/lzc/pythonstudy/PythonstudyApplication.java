343ba72cd12c6178fc1d86b8d224f92c976d2872
qiqi <2046919969@qq.con>
2018-11-21 18:09:32 +0800

    Python创建项目提交

---
+++ src/main/java/com/lzc/pythonstudy/PythonstudyApplication.java
@@ -0,0 +1,12 @@
+package com.lzc.pythonstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PythonstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PythonstudyApplication.class, args);
+    }
+}
---
+++ src/test/java/com/lzc/pythonstudy/PythonstudyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lzc.pythonstudy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PythonstudyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
